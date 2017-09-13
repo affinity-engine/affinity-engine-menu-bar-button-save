8b05df6e63e7c606c97128e2af1a1b6eba72bfe8
null null null <null@affinitygamescollective.org>
2017-09-13 13:21:55 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/menu-bar/button/save.js
+++ addon/affinity-engine/configs/menu-bar/button/save.js
@@ -1,16 +1,18 @@
 export default {
   priority: 2,
-  component: {
-    menuBar: {
-      button: {
-        save: {
-          attrs: {
-            header: 'affinity-engine.menu-bar.buttons.save.header',
-            icon: {
-              type: 'upload'
-            },
-            menu: {
-              columns: '2'
+  default: {
+    component: {
+      menuBar: {
+        button: {
+          save: {
+            attrs: {
+              header: 'affinity-engine.menu-bar.buttons.save.header',
+              icon: {
+                type: 'upload'
+              },
+              menu: {
+                columns: '2'
+              }
             }
           }
         }
